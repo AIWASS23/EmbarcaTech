f7c07c89411d4acfa2331d7a58741eba23e17df1
AIWASS23 <marcelodearaujo677@gmail.com>
2024-12-04 13:30:53 -0300

    Estudos de casos

---
+++ unidade4.c
@@ -0,0 +1,37 @@
+/* 
+
+Leds RGB
+Vm - GPIO13
+Az - GPIO12
+Vd - GPIO11
+
+Comunicação SPI
+TX - GP19
+Rx - GPIO16
+CSn - GPIO17
+SCK - GP18
+
+Display OLED
+SDA - GPIO14
+SCL - GPIO15
+
+Botões
+GPIO5 e GPIO6
+
+Buzzer
+GPIO10 e GPIO21
+
+Matriz de Leds Coloridos
+GPIO7, 5 linhas por 5 colunas
+
+Microfone
+GP28
+
+Joystick
+Vry - GPIO26
+Vrx - GPIO27
+SW - GPIO22
+
+*/
+
+
